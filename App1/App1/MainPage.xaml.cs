f577d6f809f52d13afbd672fa5dfcb1c0d3e547b
Souhail99 <55179344+Souhail99@users.noreply.github.com>
2021-03-04 17:48:54 +0100

    Ajoutez des fichiers projet.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPressed(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = "Pressed";
+            button.BackgroundColor = Color.White;
+            Content = new View1();
+        }
+
+        private void OnReleased(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = "Realeased";
+            button.BackgroundColor = Color.Salmon;
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = "Pressed";
+            button.BackgroundColor = Color.White;
+            Content = new View1();
+        }
+
+        public void Alpha()
+        {
+            Random rd = new Random();
+            bool passe = false;
+            System.Threading.Thread.Sleep(50);
+            Task.Delay(rd.Next(10000, 20000)).ContinueWith(_ =>
+            {
+                if (commande.EtatCommande == "En préparation")
+                {
+                    commande.EtatCommande = "En livraison";
+                    passe = true;
+                }
+                int index = 0;
+                if (passe)
+                {
+                    for (int i = 0; i < livreurs.Count; i++)
+                    {
+                        if (livreurs[i].Commandedulivreur.NumeroDeCommande == commande.NumeroDeCommande)
+                        {
+                            index = i;
+                        }
+                        else { }
+                    }
+                    livreurs[index].EtatLivraison();
+                    tb.Text = "En livraison";
+                }
+                passe = false;
+                System.Threading.Thread.Sleep(50);
+                Task.Delay(rd.Next(20000, 30000)).ContinueWith(x =>
+                {
+                    if (commande.EtatCommande == "En livraison")
+                    {
+                        commande.EtatCommande = "Arrivée";
+                        passe = true;
+                    }
+                    if (passe)
+                    {
+                        livreurs[index].Etat = "Sur place";
+                        tb.Text = "Arrivée";
+                    }
+                }
+                );
+            }
+            );
+
+
+
+            Task.Delay(50000).ContinueWith(_ =>
+            {
+                if (commande.EtatCommande == "Arrivée")
+                {
+                    this.com.Remove(commande);
+                    ltb.Remove(tb);
+                    ltb2.Remove(tb2);
+                    ltb3.Remove(tb3);
+                    ltb4.Remove(tb4);
+                    ltb5.Remove(tb5);
+                }
+            }
+           );
+
+
+        }
+    }
+}
---
+++ App1/App1/PageAdmin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageAdmin : ContentView
+    {
+        public PageAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
